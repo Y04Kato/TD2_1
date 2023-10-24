b02abce7caf6d38a2af191f323cb928931e36e1e
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-10-24 16:49:56 +0900

    最新更新用だミー

---
+++ project/gamedata/Target.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdint.h>
+#include <array>
+#include <list>
+#include "VectorInt.h"
+#include <memory>
+#include "components/manager/TextureManager.h"
+#include "components/2d/CreateSprite.h"
+class Target {
+public:
+	void SetWorldTransform();
+private:
+};
